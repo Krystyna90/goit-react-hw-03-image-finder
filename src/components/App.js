b154509d4aea58eeb08cab9d562559a0e8d2b93b
ukrbride4u <ukrbride4u@gmail.com>
2022-09-27 12:17:55 +0300

    the end

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,139 @@
 import React, { Component } from "react";
+import fetchImages from "../fetch/images-api";
+import Searchbar from "./Searchbar/Searchbar";
+import ImageGallery from "./ImageGallery/ImageGallery";
+import LoadMore from "./LoadMore/LoadMore";
+import Loader from "./Loader/Loader";
 import Modal from "./Modal/Modal";
+import css from "./App.module.css";
 
 class App extends Component {
   state = {
+    imageName: "",
+    page: 1,
+    imagesOnPage: 0,
+    totalImages: 0,
+    isLoading: false,
     showModal: false,
+    images: null,
+    error: null,
+    currentImageUrl: null,
+    currentImageDescription: null,
   };
+
+  componentDidUpdate(prevProps, prevState) {
+    const { imageName, page } = this.state;
+
+    if (prevState.imageName !== imageName) {
+      this.setState(({ isLoading }) => ({ isLoading: !isLoading }));
+
+      fetchImages(imageName)
+        .then(({ hits, totalHits }) => {
+          const imagesArray = hits.map((hit) => ({
+            id: hit.id,
+            description: hit.tags,
+            smallImage: hit.webformatURL,
+            largeImage: hit.largeImageURL,
+          }));
+
+          return this.setState({
+            page: 1,
+            images: imagesArray,
+            imagesOnPage: imagesArray.length,
+            totalImages: totalHits,
+          });
+        })
+        .catch((error) => this.setState({ error }))
+        .finally(() =>
+          this.setState(({ isLoading }) => ({ isLoading: !isLoading }))
+        );
+    }
+
+    if (prevState.page !== page && page !== 1) {
+      this.setState(({ isLoading }) => ({ isLoading: !isLoading }));
+
+      fetchImages(imageName, page)
+        .then(({ hits }) => {
+          const imagesArray = hits.map((hit) => ({
+            id: hit.id,
+            description: hit.tags,
+            smallImage: hit.webformatURL,
+            largeImage: hit.largeImageURL,
+          }));
+
+          return this.setState(({ images, imagesOnPage }) => {
+            return {
+              images: [...images, ...imagesArray],
+              imagesOnPage: imagesOnPage + imagesArray.length,
+            };
+          });
+        })
+        .catch((error) => this.setState({ error }))
+        .finally(() =>
+          this.setState(({ isLoading }) => ({ isLoading: !isLoading }))
+        );
+    }
+  }
+
+  getSearchRequest = (imageName) => {
+    this.setState({ imageName });
+  };
+
+  onNextFetch = () => {
+    this.setState(({ page }) => ({ page: page + 1 }));
+  };
+
   toggleModal = () => {
-    this.setState(({ showModal }) => ({
-      showModal: !showModal,
-    }));
+    this.setState(({ showModal }) => ({ showModal: !showModal }));
   };
+
+  openModal = (e) => {
+    const currentImageUrl = e.target.dataset.large;
+    const currentImageDescription = e.target.alt;
+
+    if (e.target.nodeName === "IMG") {
+      this.setState(({ showModal }) => ({
+        showModal: !showModal,
+        currentImageUrl: currentImageUrl,
+        currentImageDescription: currentImageDescription,
+      }));
+    }
+  };
+
   render() {
-    const { showModal } = this.state;
+    const {
+      images,
+      imagesOnPage,
+      totalImages,
+      isLoading,
+      showModal,
+      currentImageUrl,
+      currentImageDescription,
+    } = this.state;
+
+    const getSearchRequest = this.getSearchRequest;
+    const onNextFetch = this.onNextFetch;
+    const openModal = this.openModal;
+    const toggleModal = this.toggleModal;
+
     return (
-      <div className="App">
-        <button type="button" onClick={this.toggleModal}>
-          Open Modal
-        </button>
+      <div>
+        <Searchbar onSubmit={getSearchRequest} />
+
+        {images && <ImageGallery images={images} openModal={openModal} />}
+
+        {isLoading && <Loader />}
+
+        {imagesOnPage >= 12 && imagesOnPage < totalImages && (
+          <LoadMore onNextFetch={onNextFetch} />
+        )}
+
         {showModal && (
-          <Modal onClose={this.toggleModal}>
-            <button type="button" onClick={this.toggleModal}>
-              X
-            </button>
-          </Modal>
+          <Modal
+            onClose={toggleModal}
+            currentImageUrl={currentImageUrl}
+            currentImageDescription={currentImageDescription}
+          />
         )}
       </div>
     );
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import { createPortal } from "react-dom";
 import css from "./Modal.module.css";
 
 const modalRoot = document.querySelector("#modal-root");
 
 export default class Modal extends Component {
+  static propTypes = {
+    title: PropTypes.string,
+    onClose: PropTypes.func.isRequired,
+    currentImageUrl: PropTypes.string,
+    currentImageDescription: PropTypes.string,
+  };
+
   componentDidMount() {
     window.addEventListener("keydown", this.handleKeyDown);
   }
@@ -24,9 +32,22 @@ export default class Modal extends Component {
   };
 
   render() {
+    const {
+      title,
+      onClose,
+      currentImageUrl,
+      currentImageDescription,
+    } = this.props;
+
     return createPortal(
       <div className={css.Overlay} onClick={this.handleBackdropClick}>
-        <div className={css.Modal}>{this.props.children}</div>
+        <div className={css.Modal}>
+          <img
+            src={currentImageUrl}
+            alt={currentImageDescription}
+            loading="lazy"
+          />
+        </div>
       </div>,
       modalRoot
     );
